8a42912c2cc01b20ac11c0f1d1093955bf68b0f9
marbsouza <mariana.barbosa@g.globo>
2023-10-13 10:51:37 -0300

    feature: creating chessboard and chess layer

--- chess_game/src/Application/Main.java
+++ chess_game/src/Application/Main.java
@@ -2,10 +2,12 @@ package Application;
 
 import BoardGame.Board;
 import BoardGame.Position;
+import Chess.ChessMatch;
 
 public class Main {
     public static void main(String[] args) {
-        Board board = new Board(8,8);
-       
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
+
     }
 }
