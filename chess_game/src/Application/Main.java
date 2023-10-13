93f4e17883b0b017232d87f284a6423c8d8032c4
marbsouza <mariana.barbosa@g.globo>
2023-10-13 10:32:19 -0300

    feature: Adding new boardgame classes as Board, Piece and Position

---
+++ chess_game/src/Application/Main.java
@@ -0,0 +1,11 @@
+package Application;
+
+import BoardGame.Board;
+import BoardGame.Position;
+
+public class Main {
+    public static void main(String[] args) {
+        Board board = new Board(8,8);
+       
+    }
+}
---
+++ chess_game/src/BoardGame/Position.java
@@ -0,0 +1,32 @@
+package BoardGame;
+
+public class Position {
+
+    private int row;
+    private int column;
+
+    public Position(int row, int collumn) {
+        this.row = row;
+        this.column = collumn;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+    @Override
+    public String toString(){
+        return row + ", " + column;
+    }
+}
